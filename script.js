c0306f095adf04541c5f1659be3bd2d49ea4f34a
StephanyGarcesGuerra <stephanygarces96@gmail.com>
2025-04-06 15:57:34 -0400

    getElementById() means to get an html element by calling the id given to it

---
+++ script.js
@@ -0,0 +1,54 @@
+let titleElement = document.getElementById("title");
+
+console.log(titleElement);
+
+
+
+
+// const cat = document.getElementById('cat')
+// const dog = document.getElementById('dog')
+// const panda = document.getElementById('panda')
+
+// const cat = document.querySelector('h3')
+// const tiger = document.querySelector('h3#tiger')
+// const puma =  document.querySelector('h3#puma')
+// console.log(cat);
+// console.log(dog);
+// console.log(panda);
+// console.log(cat);
+// console.log(tiger);
+// console.log(puma);
+
+
+// const h2Tags = document.querySelectorAll("h2")
+// console.log(h2Tags);
+
+
+// const pTags =  document.querySelectorAll("p")
+// console.log(pTags);
+
+// const h3tag = document.querySelectorAll("h3")
+
+// h3tag.innerHTML = "False or true? CATS"
+// console.log(h3tag);
+//  const pTag = document.querySelector('p')
+//  console.log(pTag);
+
+//  document.querySelector('p').innerText = "Pass or fail?"
+//  pTag.innerHTML = "second times the charm"
+//  document.querySelector('p#money').innerText = "Third attempt?"
+
+// let dogs = document.getElementById('dogs')
+// dogs.style.height = "100px"
+
+// let newH1El = document.createElement("H1")
+// newH1El.innerText = "Hello new element"
+// console.log(newH1El);
+
+// let newDiv = document.createElement("div")
+// newDiv.innerText = "Hi div"
+// console.log(newDiv);
+
+// document.body.append(newDiv)
+// document.body.append(newH1El)
+
